7026e5edafa0ac48419e14f5f7de0fc7e6623f1f
Priyanshu Kumar <priyanshu00268@gmail.com>
2025-08-30 07:37:20 +0000

    testing deployment

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	err := godotenv.Load("../../.env")
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error related to env variables:", err)
 	}
 
 	serverPort := os.Getenv("SERVER_PORT")
